2e717c67d62296a55464019471c36ea6ec097c8b
Will Compton <willrandolphcompton@gmail.com>
2013-08-10 00:19:35 -0600

    Links css/gumby.css in framework.info

--- html.tpl.php
+++ html.tpl.php
@@ -20,6 +20,7 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 
 </html>
